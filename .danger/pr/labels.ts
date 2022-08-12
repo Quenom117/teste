b19adaad002d28e5306aeec980629edd9cd9d1c8
Thales Menezes <thlmenezes@pm.me>
2022-08-12 12:40:21 -0300

    ♻️ (danger): plugin labels upgrade

--- .danger/pr/labels.ts
+++ .danger/pr/labels.ts
@@ -1,5 +1,4 @@
-/* eslint-disable */
-// Provides dev-time type structures for  `danger` - doesn't affect runtime.
+// Source: https://github.com/withspectrum/danger-plugin-labels/blob/master/src/index.ts
 import { DangerDSLType } from 'danger/distribution/dsl/DangerDSL';
 
 declare let danger: DangerDSLType;
@@ -16,10 +15,25 @@ interface Options {
   validate?: (labels: string[]) => Promise<boolean> | boolean;
 }
 
-const CHECKBOXES = /^[\t ]*-[\t ]*\[x\][\t ]*(.+?)$/gim;
-const UNCHECKEDBOXES = /^[\t ]*-[\t ]*\[[\t ]*\][\t ]*(.+?)$/gim;
+interface IGitHub {
+  issue: {
+    body?: string;
+    number?: number;
+  };
+  repository?: {
+    name: string;
+    owner: {
+      login: string;
+    };
+  };
+}
 
+/**
+ * Extract checked text from markdown checkbox
+ */
 const getCheckedBoxes = (text: string): string[] => {
+  const CHECKBOXES = /^[\t ]*-[\t ]*\[x\][\t ]*(.+?)$/gim;
+
   // Full Text => ["- [x] Checked", "- [x] Also Checked"]
   const rawMatches = text.match(CHECKBOXES);
   if (!rawMatches || rawMatches.length === 0) {
@@ -34,14 +48,19 @@ const getCheckedBoxes = (text: string): string[] => {
     .map((res) => res && res[1]) as string[];
 };
 
+/**
+ * Extract checked text from markdown checkbox
+ */
 const getUncheckedBoxes = (text: string): string[] => {
+  const UNCHECKEDBOXES = /^[\t ]*-[\t ]*\[[\t ]*\][\t ]*(.+?)$/gim;
+
   // Full Text => ["- [ ] Unchecked", "- [  ] Also Unchecked"]
   const rawMatches = text.match(UNCHECKEDBOXES);
   if (!rawMatches || rawMatches.length === 0) {
     return [];
   }
 
-  // Extract checked text from markdown checkbox
+  // Extract unchecked text from markdown checkbox
   // "- [ ] Unchecked" => "Unchecked"
   return rawMatches
     .map((result) => new RegExp(UNCHECKEDBOXES.source, 'mi').exec(result))
@@ -50,17 +69,59 @@ const getUncheckedBoxes = (text: string): string[] => {
 };
 
 /**
- * Let any user add a certain set of labels to your issues and pull requests
+ * Retrieves body and issue information, from PR and Issues alike
  */
-export async function labels(options: Options) {
-  if (!options || !options.rules) {
-    throw new Error(
-      '[danger-plugin-labels] Please specify the "rules" option.'
-    );
+function getIssue() {
+  // PR
+  if (danger.github.thisPR) {
+    const pr = danger.github.thisPR;
+    return {
+      body: danger.github.pr.body,
+      issue: { issue_number: pr.number, repo: pr.repo, owner: pr.owner },
+    };
   }
-  const { rules: input, validate } = options;
+  // Issue
+  const gh = danger.github as IGitHub;
+  return {
+    body: gh.issue.body ?? '',
+    issue: {
+      issue_number: gh.issue.number ?? 0,
+      repo: gh.repository?.name ?? '',
+      owner: gh.repository?.owner.login ?? '',
+    },
+  };
+}
+// NOTE: next two functions could be one?
+/**
+ * Filter issue body checked labels, extracted from checkboxes, using rules
+ */
+function getMatchingLabels(body: string, rules: Rule[]) {
+  return getCheckedBoxes(body)
+    .map((label) => {
+      const rule = rules.find((r) => r.match.test(label));
+      return rule?.label;
+    })
+    .filter(Boolean) as string[];
+}
 
-  const rules = input.map((rule) => {
+/**
+ * Filter issue body unchecked labels, extracted from checkboxes, using rules
+ */
+function getUncheckedLabels(body: string, rules: Rule[]) {
+  return getUncheckedBoxes(body).reduce((labels, label) => {
+    const rule = rules.find((r) => r.match.test(label));
+    if (!rule) {
+      return labels;
+    }
+    return [...labels, label]; // WARNING: possible bug, should `rules.label`?
+  }, [] as string[]);
+}
+
+/**
+ * Create Rules[] from RuleInput[]
+ */
+function parseRuleInput(rules: RuleInput[]) {
+  return rules.map((rule) => {
     if (typeof rule !== 'string') {
       return rule;
     }
@@ -70,59 +131,41 @@ export async function labels(options: Options) {
       label: rule,
     };
   }) as Rule[];
+}
 
-  const api = danger.github.api;
-  let issue = { issue_number: 0, repo: '', owner: '' };
-  const existingLabels = danger.github.issue.labels.map(({ name }) => name);
-  let text = '';
-  // PR
-  if (danger.github.thisPR) {
-    const pr = danger.github.thisPR;
-    text = danger.github.pr.body;
-    issue = { issue_number: pr.number, repo: pr.repo, owner: pr.owner };
-    // Issue
-  } else {
-    const gh = danger.github as any;
-    text = gh.issue.body;
-    issue = {
-      issue_number: gh.issue.number,
-      repo: gh.repository.name,
-      owner: gh.repository.owner.login,
-    };
+/**
+ * Let any user add a certain set of labels to your issues and pull requests
+ */
+export async function labelsPlugin(options: Options) {
+  if (!options || !options.rules) {
+    throw new Error(
+      '[danger-plugin-labels] Please specify the "rules" option.'
+    );
   }
 
-  const matchingLabels = getCheckedBoxes(text)
-    .map((label) => {
-      const rule = rules.find((r) => r.match.test(label));
-      if (!rule) {
-        return null;
-      }
-      return rule.label;
-    })
-    .filter(Boolean) as string[];
+  const { body, issue } = getIssue();
 
-  const uncheckedLabels = getUncheckedBoxes(text).reduce((labels, label) => {
-    const rule = rules.find((r) => r.match.test(label));
-    if (!rule) {
-      return labels;
-    }
-    return [...labels, label];
-  }, []);
+  const rules = parseRuleInput(options.rules);
+  const matchingLabels = getMatchingLabels(body, rules);
+  const uncheckedLabels = getUncheckedLabels(body, rules);
 
   if (matchingLabels.length === 0 && uncheckedLabels.length === 0) {
     return;
   }
 
-  if (validate && (await validate(matchingLabels)) === false) {
+  if (options.validate && (await options.validate(matchingLabels)) === false) {
     return;
   }
 
-  const replacementLabels = [
-    ...matchingLabels,
-    ...existingLabels.filter((label) => uncheckedLabels.indexOf(label) === -1),
-  ].filter((item, pos, ar) => ar.indexOf(item) === pos) as string[];
+  const existingLabels = danger.github.issue.labels.map(({ name }) => name);
+  const replacementLabels = Array.from(
+    new Set([
+      ...matchingLabels,
+      ...existingLabels.filter((label) => uncheckedLabels.includes(label)),
+    ])
+  ) as string[];
 
-  await api.issues.setLabels({
+  await danger.github.api.issues.setLabels({
     ...issue,
     labels: replacementLabels,
   });
